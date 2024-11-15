b8cc4c9315a14b65aace15c768d34a9256f7c583
Dasha Stashkevich <stash.dashawork@gmail.com>
2024-11-15 17:19:04 +0300

    add new version express-handlebars

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const { engine } = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const path = require('path');
+
+
 
 
 const app = express();
@@ -9,7 +11,7 @@ const port = 8580;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.engine('handlebars', engine());
+app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
